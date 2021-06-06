320084232e4b2272494fe2201688f264a4e0269f
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-05 23:29:50 -0400

    Completed code.

--- src/main/java/ex22/App.java
+++ src/main/java/ex22/App.java
@@ -4,8 +4,34 @@
  */
 package ex22;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args){
-        
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the first number: ");
+        double first = in.nextDouble();
+
+        System.out.print("Enter the second number: ");
+        double second = in.nextDouble();
+
+        System.out.print("Enter the third number: ");
+        double third = in.nextDouble();
+
+        if(first == second || first == third || second == third) {
+            System.out.println("Error. Numbers must all have different values.");
+            System.exit(0);
+        }
+
+        double largest;
+        if(first > second && first > third) {
+            largest = first;
+        }else if(second > first && second > third){
+            largest = second;
+        }else
+            largest = third;
+
+        System.out.println("The largest number is " + largest + ".");
     }
 }
