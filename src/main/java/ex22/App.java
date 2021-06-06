47f8fe4eced413440e9c5fad2a2f34d5cf65e0a9
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-05 23:18:51 -0400

    Initial commit

---
+++ src/main/java/ex22/App.java
@@ -0,0 +1,11 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Evan Raiford
+ */
+package ex22;
+
+public class App {
+    public static void main(String[] args){
+        
+    }
+}
